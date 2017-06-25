f51b2af21e82696f05a25bd139aa7a9e9fe7dad8
Illia <petrashillia@gmail.com>
2017-06-25 11:14:28 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+//literal notation
+var snoopy ={
+    species: "beagle",
+    age: 10
+    };
+
+//constructor notation
+var buddy = new Object();
+buddy.age = 5;
+buddy.species = "golden retriever";
