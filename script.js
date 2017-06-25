bcc0fea4cebb4c6cc393b1092fc71fa9e839b1c8
Illia <petrashillia@gmail.com>
2017-06-25 13:23:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //literal notation
+
 var snoopy ={
     species: "beagle",
     age: 10
@@ -8,3 +9,21 @@ var snoopy ={
 var buddy = new Object();
 buddy.age = 5;
 buddy.species = "golden retriever";
+
+
+
+
+
+
+//Custom Constructors
+
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+}
+
+// Let's make bob and susan again, using our constructor
+var bob = new Person("Bob Smith", 30);
+var susan = new Person("Susan Jordan", 25);
+// help us make george, whose name is "George Washington" and age is 275
+var george = new Person("George Washington", 275);
